579ebe173886add5657d08d17a975491cff2bd8e
LukasG85 <lukas85b@gmail.com>
2019-10-22 12:21:48 +0200

    Updates

---
+++ precache-manifest.8a287b73d0d27f056bdcdeb5a9f511d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0cc6b225dc163df31d8c8b159e1cf5",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "6924560346e3ba39ec9e",
+    "url": "/currency/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "baf2c970757a94b6990c",
+    "url": "/currency/static/css/main.423ed580.chunk.css"
+  },
+  {
+    "revision": "6924560346e3ba39ec9e",
+    "url": "/currency/static/js/2.e8ad308b.chunk.js"
+  },
+  {
+    "revision": "baf2c970757a94b6990c",
+    "url": "/currency/static/js/main.ad266bbd.chunk.js"
+  },
+  {
+    "revision": "98a1df53251c8ea4f60c",
+    "url": "/currency/static/js/runtime-main.c029135e.js"
+  },
+  {
+    "revision": "7cf33ffd30f3760b14d5ac603db47ff0",
+    "url": "/currency/static/media/euro3.7cf33ffd.jpg"
+  }
+]);
